627fb2db5c530f99e0df92463cc4fb9b74a47042
9979Gyu <yuqin1161@gmail.com>
2025-01-12 16:32:14 +0800

    update

--- script/script.js
+++ script/script.js
@@ -7,4 +7,16 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
 
     });
+
+    $('#navbar-placeholder').load('header.html', function(response, status, xhr) {
+        if (status == "error") {
+            console.error("Error loading the navbar:", xhr.statusText);
+        }
+    });
+
+    $('#footer-placeholder').load('footer.html', function(response, status, xhr) {
+        if (status == "error") {
+            console.error("Error loading the footer:", xhr.statusText);
+        }
+    });
 });
