f8def8299562d85ef480332cd27fe7fb6b472bc0
9979Gyu <yuqin1161@gmail.com>
2025-01-12 16:25:40 +0800

    update

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            By <a href="https://jgthms.com">Gui Yu Qin</a> | B032220008 | yuqin1161@gmail.com | 2025
+        </p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,45 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+            <strong class="black-ops-one-regular big">G.Y.Q.</strong>
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>    
+        
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="index.php">Home</a>
+            <a class="navbar-item" href="about.php">About</a>
+            <a class="navbar-item" href="education.php">Qualification</a>
+            <a class="navbar-item" href="project.php">Project</a>
+            <a class="navbar-item" href="strength.php">Strength</a>
+    
+            <div class="navbar-item has-dropdown is-hoverable"> 
+                <a class="navbar-link">More</a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="interest.php">Interest</a>
+                    <a class="navbar-item" href="reference.php">References</a>
+                    <hr class="navbar-divider">
+                    <a class="navbar-item" href="report.php">Report an issue</a>
+                </div>
+            </div>
+        </div>
+    
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-primary" href="https://www.linkedin.com/in/guiyuqin" target="_blank">
+                        <strong>LinkedIn</strong>
+                    </a>
+                    <a class="button is-light" href="mailto:yuqin1161@gmail.com">Send Email</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
