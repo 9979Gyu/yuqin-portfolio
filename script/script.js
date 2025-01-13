be9044c9c37227561319cbafd184fc653dccb1df
9979Gyu <yuqin1161@gmail.com>
2025-01-13 22:59:48 +0800

    update

--- script/script.js
+++ script/script.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-
-    });
 
     $('#navbar-placeholder').load('header.html', function(response, status, xhr) {
         if (status == "error") {
