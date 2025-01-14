5c6efd4d8f62a27c9ec2b4eeb561f97f18eeb45a
9979Gyu <yuqin1161@gmail.com>
2025-01-15 00:37:50 +0800

    update education

--- script/script.js
+++ script/script.js
@@ -11,4 +11,27 @@ $(document).ready(function() {
             console.error("Error loading the footer:", xhr.statusText);
         }
     });
+
+    $(".education button").click(function(){
+        var icon = $(this).find('i');
+        var cardContent = $(this).closest('.education').find('.card-content');
+        cardContent.toggleClass('is-hidden is-show');
+        icon.toggleClass('fa-angle-down fa-angle-up');
+    });
+
+    $(".tabs li").click(function(e) {
+        e.preventDefault(); // Prevent default anchor behavior
+
+        // Remove "is-active" class from all tabs and add to the clicked one
+        $(".tabs li").removeClass("is-active");
+        $(this).addClass("is-active");
+
+        // Get the target content ID from the clicked tab's href
+        var target = $(this).find("a").attr("href").substring(1); // "acedemic" or "achievement"
+
+        // Hide all tab content and show the corresponding one
+        $(".tab-content").removeClass("is-show").addClass("is-hidden");
+        $("#" + target).removeClass("is-hidden").addClass("is-show");
+    });
+
 });
